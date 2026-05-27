37da763b26676573bbf1fc72c3145ce931b11165
park1394 <qkrwnl0330@gmail.com>
2026-05-27 16:03:06 +0900

    깃 커밋 테스트

--- src/test/java/com/busanit501/busanit501ec2/Busanit501Ec2ApplicationTests.java
+++ src/test/java/com/busanit501/busanit501ec2/Busanit501Ec2ApplicationTests.java
@@ -1,13 +1,36 @@
 package com.busanit501.busanit501ec2;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 @SpringBootTest
 class Busanit501Ec2ApplicationTests {
 
+    @Autowired
+    private DataSource dataSource;
+
     @Test
     void contextLoads() {
+
+        try(Connection connection = dataSource.getConnection();
+            PreparedStatement preparedStatement = connection.
+                    prepareStatement("select now()");
+            ResultSet resultSet = preparedStatement.executeQuery();
+        ){
+            resultSet.next();
+
+            System.out.println(resultSet.getString(1));
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
     }
 
 }
